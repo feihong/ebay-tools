ef295f7504d952d65f4c6958835cabc4658d1ced
Feihong <feihong.hsu@gmail.com>
2016-07-19 16:20:00 -0500

    Add model to the print messages and the report

--- check_orders.py
+++ check_orders.py
@@ -9,28 +9,9 @@ import requests
 from mako.template import Template
 from plim import preprocessor
 from ebaysdk.trading import Connection as Trading
+from ebaysdk.shopping import Connection as Shopping
 
 import config
-
-
-def get_items(order):
-    for transaction in order.TransactionArray.Transaction:
-        yield transaction.Item
-
-
-def get_address(order):
-    sa = order.ShippingAddress
-    addr = (sa.Name, sa.Street1, sa.Street2, sa.CityName, sa.StateOrProvince,
-            sa.CountryName, sa.PostalCode)
-    return '\n'.join(line for line in addr if line is not None and line.strip())
-
-
-
-def send_text(order_count):
-    data = dict(
-        number=config.SMS_NUMBER,
-        message='%d orders awaiting shipment' % order_count)
-    requests.post('http://textbelt.com/text', data)
 
 
 def get_orders():
@@ -56,7 +37,7 @@ def get_orders():
         print('Paid %s on %s' % (order.AmountPaid.value, order.PaidTime))
         print('Items:')
         for item in get_items(order):
-            print('- ' + item.Title)
+            print('- %s => %s' % (item.Title, item.Model))
         print('Address:')
         print(get_address(order))
         print('='*80)
@@ -66,8 +47,44 @@ def get_orders():
     print('%d orders awaiting shipment' % count)
 
 
+def get_items(order):
+    for transaction in order.TransactionArray.Transaction:
+        item = transaction.Item
+        item.Model = get_model(item)
+        yield item
+
+
+def get_model(item):
+    api = Shopping(config_file=None, **config.credentials)
+    response = api.execute('GetSingleItem', {
+        'ItemID': item.ItemID,
+        'IncludeSelector': 'ItemSpecifics'
+    })
+    item_specifics = response.reply.Item.ItemSpecifics.NameValueList
+    for spec in item_specifics:
+        if spec.Name == 'Model':
+            return spec.Value
+    return None
+
+
+def get_address(order):
+    sa = order.ShippingAddress
+    addr = (sa.Name, sa.Street1, sa.Street2, sa.CityName, sa.StateOrProvince,
+            sa.CountryName, sa.PostalCode)
+    return '\n'.join(line for line in addr if line is not None and line.strip())
+
+
+
+def send_text(order_count):
+    data = dict(
+        number=config.SMS_NUMBER,
+        message='%d orders awaiting shipment' % order_count)
+    requests.post('http://textbelt.com/text', data)
+
+
 if __name__ == '__main__':
     orders = list(get_orders())
     tmpl = Template(filename='check_orders.plim', preprocessor=preprocessor)
     with open('report.html', 'w') as fp:
-        fp.write(tmpl.render(orders=orders, get_items=get_items))
+        html = tmpl.render(orders=orders, get_items=get_items, get_address=get_address)
+        fp.write(html)
