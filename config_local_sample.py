48faf155386f0292c9d93f40ace780f0b76ede64
Feihong Hsu <feihong.hsu@gmail.com>
2016-09-21 19:35:07 -0500

    Add new sample config file

---
+++ config_local_sample.py
@@ -0,0 +1,14 @@
+# Copy this file, rename it to config_local.py and fill with appropriate values.
+
+SMS_NUMBER = '+18881112222'
+
+EMAIL_ADDRESS = 'megaman@gmail.com'
+
+AWS_PARAMS = 'aws_access_key_id;aws_secret_access_key'
+
+MAILGUN_PARAMS = 'domain.com;privay_key'
+
+EBAY_PARAMS = [
+    ('user1', 'appid;devid;certid;token'),
+    ('user2', 'appid;devid;certid;token'),
+]
--- file_config_sample.py
+++
@@ -1,6 +0,0 @@
-EBAY_PARAMS = 'XXX;YYY;ZZZ;000'
-SMS_NUMBER = '5551234567'
-REPORT_PATH = '~/webapps/static/ebay/report.html'
-REPORT_URL = 'http://static.website.com/ebay/report.html'
-TIME_ZONE = 'US/Central'
-AWS_PARAMS = 'XXX;YYY'
